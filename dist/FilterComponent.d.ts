cc2cf65745f0dd21e2f6957eaa6a6187d85bfd58
Abdull-Rahman <abdul.rahman@sphereit.me>
2024-11-05 16:57:03 +0400

    downgraded react version

--- dist/FilterComponent.d.ts
+++ dist/FilterComponent.d.ts
@@ -1,3 +1,28 @@
 import React from "react";
-declare const FilterComponent: React.FC;
+type FilterValue = string | string[] | {
+    start: string;
+    end: string;
+} | number | null;
+interface FilterOption {
+    name: string;
+    type: "string" | "dateRange" | "select" | "radio" | "checkbox" | "range";
+    label: string;
+    options?: {
+        label: string;
+        value: string;
+        icon?: JSX.Element;
+    }[];
+    min?: number;
+    max?: number;
+}
+interface Filter {
+    name: string;
+    value: FilterValue;
+}
+interface FilterComponentProps {
+    filters: Filter[];
+    filterOptions: FilterOption[];
+    onFilterChange: (updatedFilters: Filter[]) => void;
+}
+declare const FilterComponent: React.FC<FilterComponentProps>;
 export default FilterComponent;
